2b9b4bf95bf28dbd1b2f1c42868cfa72b601416b
bashard001 <bdaseh@gmail.com>
2019-12-28 22:07:07 -0800

    added the prompts to allow the user to add view or remove an employee

--- employeetracker.js
+++ employeetracker.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql");
+var inquirer = require("inquirer");
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,5 +11,31 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err){
     if (err) throw err;
-    console.log("you are connected")
+    console.log("you are connected");
+    start()
 })
+
+function start(){
+    inquirer.prompt({
+        name: "edit",
+      type: "list",
+      message: "Would you like to [POST] an auction or [BID] on an auction?",
+      choices: ["add", "view", "delete"]
+    }).then(function(answer){
+
+        if (answer.edit === "add"){
+
+            addEmployee()
+
+        } else if (answer.edit === "view"){
+
+            viewEmployee()
+
+        }else{
+
+            deleteEmployee()
+
+        }
+    })
+
+}
